1de839f9cd2f9bc524310fc9467b5e28b3340abd
Andrea Hasani <pasqyra3d@gmail.com>
2019-06-20 02:44:09 +0200

    Implementing attachments

--- app/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from app.secrets import Secrets
-
-
-class Configuration:
-    SQLALCHEMY_DATABASE_URI = Secrets.SQLALCHEMY_DATABASE_URI
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
-    SECRET_KEY = os.urandom(24)
-    DEBUG = True
--- run.py
+++
@@ -1,5 +0,0 @@
-from app import socketio, application
-
-
-if __name__ == '__main__':
-    socketio.run(application, debug=True)
